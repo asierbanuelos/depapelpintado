c5c638cf91171e6dac4fed0b6242c0bfab4f81db
Asier <asierbanuelosdelafuente@gmail.com>
2026-07-03 16:07:18 +0200

    Ocultar temporalmente banner CookieYes con CSS

--- views/frontend/footer.php
+++ views/frontend/footer.php
@@ -182,6 +182,7 @@ Bitarlan
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 */
 ?>
+<style>.cky-consent-container,.cky-revisit-wrapper{display:none!important}</style>
 <script src="/js/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="/js/ajax/libs/popper.js/1.0.8/popper.min.js"></script>
 <script src="/js/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
