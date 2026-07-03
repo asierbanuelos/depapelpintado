73d923c8887146d65fa393145e277afd8a3d27d8
Asier <asierbanuelosdelafuente@gmail.com>
2026-07-03 16:28:47 +0200

    Fix base_url: cambiar www a sin-www para que los formularios POST no pierdan datos por redireccion

--- config/config.php
+++ config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://nueva.depapelpintado.es/';
-$config['base_url'] = 'https://www.depapelpintado.es/';
+$config['base_url'] = 'https://depapelpintado.es/';
 
 /*
 |--------------------------------------------------------------------------
